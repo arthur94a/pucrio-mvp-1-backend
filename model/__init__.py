6bae502570ea4e4b237cb690f724d2f10a8efc57
arthur94a <arthuraraujo94@hotmail.com>
2025-11-23 12:00:56 -0300

    Rename model Comments to Comment

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from model import Base
-from model import Comments
+from model import Comment
 
 import os
 
